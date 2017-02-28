7198c4e383fdf3af01587395e07eaf610982d76a
Sander Peerna <sanderpeerna@gmail.com>
2017-02-28 15:45:53 -0800

    Added unit tests.

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -1,20 +1,18 @@
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
 import java.text.ParseException;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by Sander Peerna on 2/28/2017.
  */
-public class UnitTest {
+public class DatabaseTest {
     SearchDatabase database;
     SearchQuery query;
 
-    public UnitTest() throws ParseException {
+    public DatabaseTest() throws ParseException {
         SearchDataParser parser = new SearchDataParser(new File("src/main/resources/test-case.json"));
         database = parser.buildSearchDatabase();
         query = parser.buildSearchQuery();
@@ -27,5 +25,32 @@ public class UnitTest {
     }
 
     @Test
-    public void updateReservations
+    public void updateCampsites() {
+        database.addCampsite(834822939, "Dave's Summer Swamp");
+        database.addCampsite(1143627, "Tucker's Clear Lake");
+
+        assertEquals(11, database.getCampsites().size());
+        assertEquals("Jonny Appleseed Log Cabin", database.getCampsite(3));
+        assertEquals("Tucker's Clear Lake", database.getCampsite(1143627));
+
+        database.removeCampsite(1143627);
+        assertNull(database.getCampsite(1143627));
+    }
+
+    @Test
+    public void updateReservations() {
+        assertFalse(database.isReserved(3, 7));
+        database.addReservation(3, 7, 9);
+        assertTrue(database.isReserved(3, 7));
+    }
+
+    @Test
+    public void updateGapRules() {
+        assertFalse(database.isGapRule(4));
+        database.addGapRule(4);
+        assertTrue(database.isGapRule(4));
+        database.removeGapRule(3);
+        assertFalse(database.isGapRule(3));
+        assertEquals(4, database.getMaxGapRule());
+    }
 }
--- src/test/java/QueryTest.java
+++ src/test/java/QueryTest.java
@@ -1,7 +1,54 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import java.io.File;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by sande on 2/28/2017.
  */
 public class QueryTest {
+    SearchDatabase database;
+    SearchQuery query;
+
+    public QueryTest() throws ParseException {
+        SearchDataParser parser = new SearchDataParser(new File("src/main/resources/test-case.json"));
+        database = parser.buildSearchDatabase();
+        query = parser.buildSearchQuery();
+    }
+
+    @Test
+    public void confirmBaseCase() {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Daniel Boone Bungalow");
+        expected.add("Teddy Rosevelt Tent Site");
+        expected.add("Bear Grylls Cozy Cave");
+        expected.add("Wyatt Earp Corral");
+        assertEquals(expected, query.findAvailableCampsites(database));
+    }
+
+    @Test
+    public void changeReservationWindow1() {
+        query.updateQuery(0, 5);
+        query.updateQuery(1, 4);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Daniel Boone Bungalow");
+        expected.add("Teddy Rosevelt Tent Site");
+        expected.add("Bear Grylls Cozy Cave");
+        expected.add("Wyatt Earp Corral");
+        assertEquals(expected, query.findAvailableCampsites(database));
+    }
+
+    @Test
+    public void changeGapRule1() {
+        database.addGapRule(1);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Daniel Boone Bungalow");
+        expected.add("Teddy Rosevelt Tent Site");
+        expected.add("Bear Grylls Cozy Cave");
+        expected.add("Wyatt Earp Corral");
+        assertEquals(expected, query.findAvailableCampsites(database));
+    }
 }
