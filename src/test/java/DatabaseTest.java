61a569d029531b7ba1ca458455ba5f935ca804fd
Sander Peerna <sanderpeerna@gmail.com>
2017-03-01 11:45:31 -0800

    Added test cases.

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -1,34 +1,45 @@
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.ParseException;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by Sander Peerna on 2/28/2017.
+ * Test the methods of the <code>CampsiteDatabase</code> class.
  */
 public class DatabaseTest {
-    SearchDatabase database;
-    SearchQuery query;
+    CampsiteDatabase database;
+    CampsiteQuery query;
 
-    public DatabaseTest() throws ParseException {
-        SearchDataParser parser = new SearchDataParser(new File("src/main/resources/test-case.json"));
+    /**
+     * Build the <code>CampsiteDatabase</code> and <code>CampsiteQuery</code> objects to use for testing.
+     * @throws ParseException
+     * @throws IOException
+     */
+    public DatabaseTest() throws ParseException, IOException {
+        CampsiteDataParser parser = new CampsiteDataParser(new File("src/main/resources/test-case.json"));
         database = parser.buildSearchDatabase();
         query = parser.buildSearchQuery();
     }
 
+    /**
+     * Confirm the database was built correctly.
+     */
     @Test
-    public void confirmDatabaseEntries() {
-        assertTrue(database.isReserved(4, 4));
-        assertFalse(database.isReserved(6, 13));
+    public void confirmBaseTestCase() {
+        assertTrue(database.isReserved(4, 156));
+        assertFalse(database.isReserved(6, 165));
     }
 
+    /**
+     * Add and remove campsites.
+     */
     @Test
     public void updateCampsites() {
         database.addCampsite(834822939, "Dave's Summer Swamp");
         database.addCampsite(1143627, "Tucker's Clear Lake");
-
         assertEquals(11, database.getCampsites().size());
         assertEquals("Jonny Appleseed Log Cabin", database.getCampsite(3));
         assertEquals("Tucker's Clear Lake", database.getCampsite(1143627));
@@ -37,13 +48,19 @@ public class DatabaseTest {
         assertNull(database.getCampsite(1143627));
     }
 
+    /**
+     * Add reservations.
+     */
     @Test
     public void updateReservations() {
-        assertFalse(database.isReserved(3, 7));
-        database.addReservation(3, 7, 9);
-        assertTrue(database.isReserved(3, 7));
+        assertFalse(database.isReserved(3, 123));
+        database.addReservation(3, 123, 126);
+        assertTrue(database.isReserved(3, 125));
     }
 
+    /**
+     * Add and remove gap sizes from to the set of gap rules.
+     */
     @Test
     public void updateGapRules() {
         assertFalse(database.isGapRule(4));
--- src/test/java/QueryTest.java
+++ src/test/java/QueryTest.java
@@ -1,26 +1,35 @@
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by sande on 2/28/2017.
+ * Test different queries for a given JSON input.
  */
 public class QueryTest {
-    SearchDatabase database;
-    SearchQuery query;
+    CampsiteDatabase database;
+    CampsiteQuery query;
 
-    public QueryTest() throws ParseException {
-        SearchDataParser parser = new SearchDataParser(new File("src/main/resources/test-case.json"));
+    /**
+     * Build the <code>CampsiteDatabase</code> and <code>CampsiteQuery</code> objects to use for testing.
+     * @throws ParseException
+     * @throws IOException
+     */
+    public QueryTest() throws ParseException, IOException {
+        CampsiteDataParser parser = new CampsiteDataParser(new File("src/main/resources/test-case.json"));
         database = parser.buildSearchDatabase();
         query = parser.buildSearchQuery();
     }
 
+    /**
+     * Query for the reservation given in the JSON input.
+     */
     @Test
-    public void confirmBaseCase() {
+    public void confirmBaseTestCase() {
         ArrayList<String> expected = new ArrayList<>();
         expected.add("Daniel Boone Bungalow");
         expected.add("Teddy Rosevelt Tent Site");
@@ -29,26 +38,77 @@ public class QueryTest {
         assertEquals(expected, query.findAvailableCampsites(database));
     }
 
+    /**
+     *  Query for a reservation from June 1 to June 2.
+     *  Assume dates not in the JSON are not reserved.
+     */
     @Test
     public void changeReservationWindow1() {
-        query.updateQuery(0, 5);
-        query.updateQuery(1, 4);
+        query.updateQuery(153, 154);
         ArrayList<String> expected = new ArrayList<>();
+        expected.add("Lewis and Clark Camp Spot");
+        expected.add("Jonny Appleseed Log Cabin");
+        expected.add("Davey Crockett Camphouse");
         expected.add("Daniel Boone Bungalow");
         expected.add("Teddy Rosevelt Tent Site");
-        expected.add("Bear Grylls Cozy Cave");
+        expected.add("Edmund Hillary Igloo");
         expected.add("Wyatt Earp Corral");
         assertEquals(expected, query.findAvailableCampsites(database));
     }
 
+    /**
+     * Query for reservation from June 10 to June 12.
+     */
+    @Test
+    public void changeReservationWindow2() {
+        query.updateQuery(162, 164);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Lewis and Clark Camp Spot");
+        expected.add("Davey Crockett Camphouse");
+        expected.add("Edmund Hillary Igloo");
+        assertEquals(expected, query.findAvailableCampsites(database));
+    }
+
+    /**
+     * Query for a reservation from June 16 to June 18.
+     */
+    @Test
+    public void changeReservationWindow3() {
+        query.updateQuery(168, 170); // June 16 - June 18
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Lewis and Clark Camp Spot");
+        expected.add("Davey Crockett Camphouse");
+        expected.add("Daniel Boone Bungalow");
+        expected.add("Bear Grylls Cozy Cave");
+        assertEquals(expected, query.findAvailableCampsites(database));
+    }
+
+    /**
+     * Query for a reservation from June 7 to June 10 with a gap rule of only 1 day.
+     */
     @Test
     public void changeGapRule1() {
         database.addGapRule(1);
+        database.removeGapRule(2);
+        database.removeGapRule(3);
         ArrayList<String> expected = new ArrayList<>();
-        expected.add("Daniel Boone Bungalow");
+        expected.add("Grizzly Adams Adventure Cabin");
+        expected.add("Jonny Appleseed Log Cabin");
         expected.add("Teddy Rosevelt Tent Site");
         expected.add("Bear Grylls Cozy Cave");
-        expected.add("Wyatt Earp Corral");
+        assertEquals(expected, query.findAvailableCampsites(database));
+    }
+
+    /**
+     * Query for a reservation from June 10 to June 11 with gap rules of 2, 3, and 4.
+     */
+    @Test
+    public void changeGapRule2() {
+        query.updateQuery(162, 163);
+        database.addGapRule(4);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("Lewis and Clark Camp Spot");
+        expected.add("Edmund Hillary Igloo");
         assertEquals(expected, query.findAvailableCampsites(database));
     }
 }
